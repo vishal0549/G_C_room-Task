87f32ad2977c6d00e58811dd6071415452f63ee9
Vishal <43312718+vishal0549@users.noreply.github.com>
2023-04-07 18:56:06 +0530

    Add files via upload

--- Task-16/src/components/A.jsx
+++ Task-16/src/components/A.jsx
@@ -6,17 +6,19 @@ const A = () => {
 
     const [ info, setInfo ] = useState("");
     const [ changeData, setChangeData ] = useState("");
+    const [ childData, setChildData ] = useState("");
 
   return (
     <div>
         <h3>A Component</h3>
         <input type="text" placeholder='Enter some Text' onChange={ (e)=> { setInfo(e.target.value) } } />
-        <button onClick={ () => setChangeData(info) }>Click me</button>
+        <button onClick={ () => setChangeData(info) }>Click me to send data in child Component</button>
+
+        <p>D Comp data : { childData }</p>
 
         <hr />
-        <hr />
 
-        <mySecondContext.Provider value={ changeData }>
+        <mySecondContext.Provider value={ { changeData, setChildData } }>
             <B />
         </mySecondContext.Provider>
         
--- Task-16/src/components/D.jsx
+++ Task-16/src/components/D.jsx
@@ -1,15 +1,21 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { mySecondContext } from '../globalData/DataProvider';
 
 const D = () => {
 
-    const dataFromA = useContext(mySecondContext);
+    const { changeData, setChildData } = useContext(mySecondContext);
+    const [ sendData, setSendData ] = useState("");
 
   return (
     <div>
         <h3>D Components</h3>
 
-        <p style={{ color: "blue" }}>Data from A : { dataFromA }</p>
+        <p style={{ color: "blue" }}>Data from A : { changeData }</p>
+
+        <hr />
+
+        <input type="text" placeholder="Enter something to send parent" onChange={ (e) => setSendData(e.target.value) } />
+        <button onClick={ () => setChildData(sendData) }>Click me to send data to A Component</button>
 
     </div>
   )
